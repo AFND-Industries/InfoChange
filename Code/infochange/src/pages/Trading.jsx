4733ed2f2d1ec4724b4097133935ea351cfddf98
Eulogio Quemada <elgameshdpay@gmail.com>
2024-04-11 19:30:13 +0200

    Chart Widget?

--- Code/infochange/src/pages/Trading.jsx
+++ Code/infochange/src/pages/Trading.jsx
@@ -1,6 +1,42 @@
-export default function Trading() {
-  return <div className="container">
-    <span className="h1">Página de trading</span><br />
-    <span>Bitcoin: 99999$</span>
-  </div>;
+import React, { useEffect, useRef } from 'react';
+
+function Trading() {
+  const container = useRef();
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+    script.type = "text/javascript";
+    script.async = true;
+    script.innerHTML = `
+      {
+        "autosize": true,
+        "symbol": "BINANCE:BTCUSDT",
+        "interval": "D",
+        "timezone": "Etc/UTC",
+        "theme": "light",
+        "style": "1",
+        "locale": "en",
+        "enable_publishing": false,
+        "allow_symbol_change": false,
+        "calendar": false,
+        "support_host": "https://www.tradingview.com"
+      }`;
+    container.current.appendChild(script);
+
+    return () => {
+      container.current.innerHTML = '';
+    };
+  }, []);
+
+  return (
+    <div className="container mt-2 mb-5" style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <div className="tradingview-widget-container" ref={container} style={{ height: "100%", width: "100%" }}>
+        <div className="tradingview-widget-container__widget" style={{ height: "calc(100% - 32px)", width: "100%" }}></div>
+        <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span className="blue-text">Track all markets on TradingView</span></a></div>
+      </div>
+    </div>
+  );
 }
+
+export default Trading;
