8c70379365aa17325860203c08dfe1958517ebe4
alers16 <aromsan@uma.es>
2024-04-15 21:02:12 +0200

    datatables

--- Code/infochange/src/pages/Coins.jsx
+++ Code/infochange/src/pages/Coins.jsx
@@ -1,3 +1,24 @@
+import React, { useEffect, useRef, useState } from 'react';
+import CoinsCap from '../data/CoinMarketCapData.json';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+
 export default function Coins() {
-  return <h1>Coins!</h1>;
+
+  useEffect(() => {
+    console.log(CoinsCap.data);
+  }
+  , []);
+
+
+  return <h1>Hola</h1>;
+
+}
+
+function getCoinDataTable(){
+  return (
+    <DataTable value={CoinsCap.data} tableStyle={{ minWidth: '50rem' }}>
+      <Column field="name" header="Nombre"></Column>
+      <Column field="symbol" header="Simbolo"></Column>
+    </DataTable>);
 }
