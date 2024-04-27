68bcec1a72984cd3824a0411ac600e76b8fcdc13
Eulogio Quemada <elgameshdpay@gmail.com>
2024-04-27 15:02:06 +0200

    Charting

---
+++ Code/infochange/src/pages/trading/components/SimpleChart.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useRef } from 'react';
+
+function SimpleChart({ symbol }) {
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js";
+        script.type = "text/javascript";
+        script.async = true;
+        script.innerHTML = JSON.stringify({
+            symbols: symbol + "|7D",
+            width: "100%",
+            height: "100%",
+            locale: "es",
+            autosize: true,
+            hideMarketStatus: true,
+            scalePosition: "right",
+            scaleMode: "Normal",
+            fontFamily: "Arial, sans-serif",
+            fontSize: "10",
+            noTimeScale: false,
+            valuesTracking: "1",
+            changeMode: "price-and-percent",
+            dateRanges: [
+                "1d|3",
+                "1w|30",
+                "1m|1H",
+                "3m|4H",
+                "6m|1D",
+                "12m|1D",
+                "all|1W"
+            ]
+        });
+        containerRef.current.appendChild(script);
+
+        return (() => {
+            if (containerRef.current != null)
+                containerRef.current.innerHTML = "";
+        });
+    }, [symbol]);
+
+    return (
+        <div className="tradingview-widget-container" ref={containerRef}></div>
+    );
+}
+
+export default SimpleChart;
--- Code/infochange/src/pages/trading/components/TradingChart.jsx
+++ Code/infochange/src/pages/trading/components/TradingChart.jsx
@@ -2,47 +2,19 @@ import React, { useEffect, useRef, useState } from 'react';
 
 import { AdvancedRealTimeChart } from "react-ts-tradingview-widgets";
 import { useTrading } from '../context/TradingContext';
+import SimpleChart from './SimpleChart';
 
 function TradingChart() {
     const { getActualPair, getChartMode, getPairPath } = useTrading();
-
-    const container = useRef();
 
     const [newbieChart, setNewbieChart] = useState(null);
     const [proChart, setProChart] = useState(null);
 
     useEffect(() => {
         if (getActualPair() !== undefined) {
-            const script = document.createElement("script");
-            script.src = "https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js";
-            script.type = "text/javascript";
-            script.async = true;
-            script.innerHTML = JSON.stringify(
-                {
-                    symbols: getActualPair().symbol + "|7D",
-                    width: "100%",
-                    height: "100%",
-                    locale: "es",
-                    autosize: true,
-                    hideMarketStatus: true,
-                    scalePosition: "right",
-                    scaleMode: "Normal",
-                    fontFamily: "Arial, sans-serif",
-                    fontSize: "10",
-                    noTimeScale: false,
-                    valuesTracking: "1",
-                    changeMode: "price-and-percent",
-                    dateRanges: [
-                        "1d|3",
-                        "1w|30",
-                        "1m|1H",
-                        "3m|4H",
-                        "6m|1D",
-                        "12m|1D",
-                        "all|1W"
-                    ]
-                });
-            container.current.appendChild(script);
+            setNewbieChart(<SimpleChart
+                symbol={getActualPair().symbol}
+            />);
 
             setProChart(<AdvancedRealTimeChart
                 locale="es"
@@ -55,11 +27,6 @@ function TradingChart() {
                 hide_side_toolbar={false}
                 allow_symbol_change={false}
             />)
-
-            return (() => {
-                if (container.current != null)
-                    container.current.innerHTML = "";
-            });
         }
     }, [getActualPair()])
 
@@ -67,9 +34,9 @@ function TradingChart() {
         <>
             {getActualPair() !== undefined ? // Si la moneda existe
                 <>
-                    <div className="border border-4 rounded tradingview-widget-container" ref={container}
+                    <div className="border border-4 rounded tradingview-widget-container"
                         style={{ height: "100%", width: "100%", display: (getChartMode() === 0 ? "block" : "none") }}>
-                        <div className="tradingview-widget-container__widget"></div>
+                        {newbieChart}
                     </div>
 
                     <div className="border border-4 rounded tradingview-widget-container"
