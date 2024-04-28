ca68c96e23d288be1edb2ea356aca0f2d4ef879a
tbms18 <124881416+tbms18@users.noreply.github.com>
2024-04-28 19:11:53 +0200

    [BACKEND] added mysql driver to express

--- Code/infochange/server/src/index.js
+++ Code/infochange/server/src/index.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql");
+require("dotenv").config();
 const app = express();
 app.use(cors());
 
+// private function
+const applog = (msg, tag) => {
+  console.log(`[${new Date().toLocaleString()}] [${tag || "INFO"}] ${msg}`);
+};
+
 const port = 1024;
+
+const db = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  applog("Conexión a la base de datos completada", "DATABASE");
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
@@ -13,6 +32,14 @@ app.get("/auth", cors(), (req, res) => {
   res.json({ status: "1" });
 });
 
+app.get("/users", (req, res) => {
+  db.query("SELECT * FROM usuario", (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
+  applog(`Petición "/users" ejecutada`, "REQUEST");
+});
+
 app.listen(port, () => {
-  console.log(`Server started on ${port} port`);
+  applog(`Servidor activo en el puerto ${port}`, "SERVER");
 });
