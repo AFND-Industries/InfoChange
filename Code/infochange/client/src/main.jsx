bc4f999633fe5dbe7f17c215dc52a522ce61aa85
Eulogio Quemada <elgameshdpay@gmail.com>
2024-04-28 17:16:00 +0200

    Authentication with Backend (Mock)

--- Code/infochange/client/src/main.jsx
+++ Code/infochange/client/src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -21,29 +21,130 @@ import "primereact/resources/primereact.min.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-const wrap = (v) => (
-  <>
-    <Header />
-    {v}
-    <Footer />
-  </>
-);
+import axios from "axios";
+
+// carpeta authenticator y mover componentes de sitio
+
+function Loading() {
+  console.log("Cargando...");
+
+  return (
+    <div className="d-flex align-items-center justify-content-center vh-100 flex-column">
+      <div className="alert alert-secondary">
+        <span className="h1">Autenticando usuario...</span>
+      </div>
+      <span>(Aún no está implementado)</span>
+    </div>
+  );
+}
+
+function ServerNotAvailable() {
+  console.log("Servidor no disponible...");
+
+  return (
+    <div className="d-flex align-items-center justify-content-center vh-100 bg-danger flex-column">
+      <div className="alert alert-danger">
+        <span className="h1">SERVIDOR NO DISPONIBLE</span>
+      </div>
+      <span className="alert alert-danger">(Si estás desarrollando, ve a main.jsx y pon un "1" en el setStatus de catch Exception para arreglarlo)</span>
+    </div>
+  );
+}
+
+function NotLogged() {
+  console.log("¡No has iniciado sesión! Estado: " + status);
+
+  return (
+    <div className="d-flex align-items-center justify-content-center vh-100 flex-column">
+      <div className="alert alert-danger">
+        <span className="h1">Usuario no autenticado</span><br />
+      </div>
+      <span>(Aquí habría que en vez de enseñar esto, redirigir a Welcome o a Login)</span>
+    </div >
+  );
+}
+
+function UnknownStatus({ status }) {
+  console.log("Status desconocido: " + status);
+
+  return (
+    <div className="d-flex align-items-center justify-content-center vh-100 flex-column">
+      <div className="alert alert-danger">
+        <span className="h1">Algo ha salido mal... Estado desconocido: {status}</span>
+      </div>
+      <span>(Contacta con el administrador y envíale una captura de esta pantalla)</span>
+    </div>
+  );
+}
+
+async function auth() {
+  return await axios.get("http://localhost:1024/auth");
+}
+
+function App() {
+  console.log("Usuario autenticado.");
+
+  const wrap = (v) => (
+    <>
+      <Header />
+      {v}
+      <Footer />
+    </>
+  );
+
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={wrap(<Welcome />)} />
+          <Route path="/coins/*" element={wrap(<Coins />)} />
+          <Route path="/dashboard/:username" element={wrap(<Dashboard />)} />
+          <Route path="/dashboard" element={wrap(<Dashboard />)} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/trading/:pair" element={wrap(<Trading />)} />
+          <Route path="/trading" element={wrap(<Trading />)} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="*" element={<Unknown />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+function Authenticator() {
+  const [status, setStatus] = useState("-2"); // -2: Loading. -1: Server not available. 0 Not logged. 1 Logged
+
+  const statusPages = {
+    "-2": <Loading />,
+    "-1": <ServerNotAvailable />,
+    "0": <NotLogged />,
+    "1": <App />
+  }
+
+  const getPage = (status) => statusPages[status] ?? <UnknownStatus status={status} />;
+
+  useEffect(() => {
+    const fetchAuth = async () => {
+      try {
+        const response = await auth();
+
+        setStatus(response.data.status);
+      } catch (Exception) {
+        console.log("El servidor no está disponible en estos momentos.");
+
+        setStatus("1"); // Pon aqui un 1 si quieres que aunque no vaya el servidor te deje entrar al front-end
+        // normalmente tiene que ser un -1, server not available 
+        //(se puede hacer que diferencie entre si tienes internet o no)
+      }
+    }
+
+    fetchAuth();
+  }, []);
+
+  return getPage(status);
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <div className="d-flex flex-column min-vh-100">
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={wrap(<Welcome />)} />
-        <Route path="/coins/*" element={wrap(<Coins />)} />
-        <Route path="/dashboard/:username" element={wrap(<Dashboard />)} />
-        <Route path="/dashboard" element={wrap(<Dashboard />)} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/trading/:pair" element={wrap(<Trading />)} />
-        <Route path="/trading" element={wrap(<Trading />)} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="*" element={<Unknown />} />
-      </Routes>
-    </BrowserRouter>
-  </div>
+  <Authenticator />
 );
--- Code/infochange/server/src/index.js
+++ Code/infochange/server/src/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 
 const port = 1024;
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
+});
+
+app.get("/auth", cors(), (req, res) => {
+  res.json({ status: "1" });
 });
 
 app.listen(port, () => {
