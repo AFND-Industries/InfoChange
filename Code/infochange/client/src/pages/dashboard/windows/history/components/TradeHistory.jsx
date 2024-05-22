870a9644aa3dccb92ca4b194d6b80a92436d0785
Eulogio Quemada <elgameshdpay@gmail.com>
2024-05-22 09:43:40 +0200

    Trade history

--- Code/infochange/client/src/pages/dashboard/windows/history/components/TradeHistory.jsx
+++ Code/infochange/client/src/pages/dashboard/windows/history/components/TradeHistory.jsx
@@ -1,6 +1,8 @@
+import React from "react";
+import TradeItem from "./TradeItem";
 import { useAPI } from "../../../../../context/APIContext";
 
-export default function TradeHistory({ tradeHistory }) {
+const TradeHistory = ({ tradeHistory }) => {
     const { getPair } = useAPI();
 
     let renderTradeHistory = null;
@@ -9,52 +11,15 @@ export default function TradeHistory({ tradeHistory }) {
         const sortedHistory = tradeHistory.sort((a, b) => new Date(b.date) - new Date(a.date));
 
         renderTradeHistory = sortedHistory.map((trade, index) => {
-            const symbol = getPair(trade.symbol);
-
-            const tradeDate = new Date(trade.date);
-
-            const hours = ('0' + tradeDate.getHours()).slice(-2);
-            const minutes = ('0' + tradeDate.getMinutes()).slice(-2);
-            const day = ('0' + tradeDate.getDate()).slice(-2);
-            const month = ('0' + (tradeDate.getMonth() + 1)).slice(-2);
-            const year = tradeDate.getFullYear();
-
-            const formattedDate = `${day}/${month}/${year} ${hours}:${minutes}`;
-
-            return (
-                <tr key={index}>
-                    <td><b>{trade.symbol}</b></td>
-                    <td>{trade.type === "BUY" ?
-                        <b className="text-success">COMPRA</b> :
-                        <b className="text-danger">VENTA</b>}</td>
-                    <td>{trade.paid_amount.toFixed(8) + " " + (trade.type === "BUY" ? symbol.quoteAsset : symbol.baseAsset)}</td>
-                    <td>{trade.amount_received.toFixed(8) + " " + (trade.type === "BUY" ? symbol.baseAsset : symbol.quoteAsset)}</td>
-                    <td>{trade.comission.toFixed(8) + " " + (trade.type === "BUY" ? symbol.quoteAsset : symbol.baseAsset)}</td>
-                    <td>{formattedDate}</td>
-                    <td>{trade.price}</td>
-                </tr>
-            );
+            return <TradeItem key={index} trade={trade} />;
         });
     }
 
     return (
-        <>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Símbolo</th>
-                        <th>Tipo</th>
-                        <th>Cantidad Pagada</th>
-                        <th>Cantidad Recibida</th>
-                        <th>Comisión</th>
-                        <th>Fecha</th>
-                        <th>Precio</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {renderTradeHistory}
-                </tbody>
-            </table>
-        </>
+        <ul className="list-group list-group-flush p-0 m-0">
+            {renderTradeHistory}
+        </ul>
     );
-}
+};
+
+export default TradeHistory;
---
+++ Code/infochange/client/src/pages/dashboard/windows/history/components/TradeItem.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { useAPI } from "../../../../../context/APIContext";
+
+const TradeItem = ({ trade }) => {
+    const { getTokenInfo, getPair } = useAPI();
+    console.log(trade);
+    const transactionDate = new Date(trade.date);
+
+    const hours = ('0' + transactionDate.getHours()).slice(-2);
+    const minutes = ('0' + transactionDate.getMinutes()).slice(-2);
+    const day = ('0' + transactionDate.getDate()).slice(-2);
+    const month = ('0' + (transactionDate.getMonth() + 1)).slice(-2);
+    const year = transactionDate.getFullYear();
+
+    const formattedDate = `${day}/${month}/${year} ${hours}:${minutes}`;
+
+    const symbol = getPair(trade.symbol);
+    const base = getTokenInfo(symbol.baseAsset);
+    const quote = getTokenInfo(symbol.quoteAsset);
+
+    const drawCoin = (coin, symbol) => (
+        <div>
+            <img style={{ width: "35px", height: "35px" }} src={coin.logo} alt={"Logo de " + symbol} />
+        </div>
+    );
+
+    const drawTransaction = (base, quote, symbol) => {
+        const coin1 = trade.type === "BUY" ? quote : base;
+        const coin2 = trade.type === "BUY" ? base : quote;
+
+        return (
+            <div className="d-flex justify-content-start align-items-center">
+                {drawCoin(coin1, trade.type === "BUY" ? symbol.quoteAsset : symbol.baseAsset)}
+                <i className="bi bi-arrow-right text-dark mx-2" style={{ fontSize: '1em' }}></i>
+                {drawCoin(coin2, trade.type === "BUY" ? symbol.baseAsset : symbol.quoteAsset)}
+            </div>
+        );
+    }
+
+    const baseAssetName = " " + symbol.baseAssetName;
+    const quoteAssetName = symbol.quoteAsset === "USDT" ? "$" : " " + symbol.quoteAssetName;
+
+    return (
+        <li key={trade.id} className="list-group-item px-0">
+            <div className="row align-items-center">
+                <div className="d-flex col-lg-9 d-flex align-items-start mb-3 mb-lg-0 flex-column">
+                    <div className="d-flex align-items-center">
+                        {drawTransaction(base, quote, symbol)}
+                    </div>
+                    <div>
+                        Has <span className="fw-bold">{trade.type === "BUY" ? "comprado" : "vendido"}&nbsp;
+                            {trade.paid_amount}{trade.type === "BUY" ? quoteAssetName : baseAssetName}</span> por&nbsp;
+                        <span className="fw-bold">{trade.amount_received}{trade.type === "BUY" ? baseAssetName : quoteAssetName}</span>
+                    </div>
+                    <div className="d-flex flex-row text-secondary flex-lg-row flex-column" style={{ fontSize: "0.9em" }}>
+                        <span>Precio: {trade.price}{quoteAssetName}&nbsp;&nbsp;</span>
+                        <span>Comisión: {trade.comission.toFixed(8)}{baseAssetName}</span>
+                    </div>
+                </div>
+                <div className="col-lg-3 d-flex flex-column align-items-center align-items-lg-end">
+                    <span className={`fw-bold me-1 ${trade.type === "BUY" ? "text-success" : "text-danger"}`}>
+                        {trade.type === "BUY" ? "COMPRA" : "VENTA"}</span>
+                    <span className="fw-bold">{trade.paid_amount}</span>
+                    <span className="text-secondary text-end" style={{ fontSize: "0.9em" }}>{formattedDate}</span>
+                </div>
+            </div>
+        </li>
+    );
+};
+
+export default TradeItem;
