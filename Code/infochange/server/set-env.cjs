f4c209570777606c7cd200e60787c4090750c69d
antBaena <antoniocanetebaena1234@gmail.com>
2024-06-06 18:19:30 +0200

    desperated update

--- Code/infochange/server/set-env.cjs
+++ Code/infochange/server/set-env.cjs
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
 let envFile;
-envFile = ".antcb";
+envFile = "prod.txt";
 fs.copyFileSync(envFile, "./bin/.env");
 console.log(`Set environment variables from ${envFile}`);
