5dc9354c9c85d15bd27e8d3f67e6fb9d5e8ad5b6
tbms18 <124881416+tbms18@users.noreply.github.com>
2024-06-06 13:52:18 +0200

    backend update

--- Code/infochange/server/app.js
+++ Code/infochange/server/app.js
@@ -5,7 +5,6 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const session = require("express-session");
 const cors = require("cors");
-const utils = require("./utils/utils");
 var indexRouter = require("./routes/index");
 
 var app = express();
@@ -22,28 +21,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
-  cors({
-    origin: "https://infochange.netlify.app",
-    credentials: true,
-  })
+    cors({
+        origin: "https://infochange.me",
+        credentials: true,
+    })
 );
 
 app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json({ error: err.message });
+    // render the error page
+    res.status(err.status || 500);
+    res.json({ error: err.message });
 });
 
 module.exports = app;
